c37f566895d7d75fd8361f0e5cba34bd87109e32
Jackie Qi <jackie@jobox.ai>
2025-08-26 19:35:27 -0700

    first commit

---
+++ Zapper/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Zapper
+//
+//  Created by Jackie Qi on 8/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ZapperTests/ZapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZapperTests.swift
+//  ZapperTests
+//
+//  Created by Jackie Qi on 8/26/25.
+//
+
+import Testing
+@testable import Zapper
+
+struct ZapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
